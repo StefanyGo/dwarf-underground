e0f9b07a20fed2c0e5834b0e6217b242d18ab4be
Stefany Go <stefany_go@hotmail.com>
2018-07-03 04:33:33 -0400

    Render the four article links at the bottom of the screen using map and passing in the props they need

--- src/Foot.js
+++ src/Foot.js
@@ -1,35 +1,51 @@
 import React, {Component} from 'react';
 
+class ClickBait extends Component {
+    render() {
+        return (
+            <div className="small-6 medium-3 columns other-article">
+              <a href="#">
+                <img src={this.props.clickBait.img} alt={this.props.clickBait.alt} />
+                <p>{this.props.clickBait.name}</p>
+              </a>
+            </div>
+        )
+    }
+}
+
 class Foot extends Component {
+    constructor() {
+        super()
+        this.state = {
+            clickBait: [
+                { name: "Single Orcs in Indianapolis",
+                  img: "https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif",
+                  alt: "orc"
+                },
+                {
+                  name: "You won't believe what's under this mountain",
+                  img: "https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg",
+                  alt: "mountain"
+                },
+                {
+                  name: "Mine 20% more gold with One Weird Trick",
+                  img: "http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg",
+                  alt: "gold"
+                },
+                {
+                  name: "Surprise for Indiana Hobbits born before 1999",
+                  img: "http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg",
+                  alt: "hobbit"
+                }
+            ]
+        }
+    }
     render() {
         return (
             <div className="small-12 columns other-articles">
             <h2>From around the Realm</h2>
-            <div className="small-6 medium-3 columns other-article">
-              <a href="#">
-                <img src="https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif" alt="orc" />
-                <p>Single Orcs in Indianapolis</p>
-              </a>
+            {this.state.clickBait.map(currentClickBait => <ClickBait clickBait={currentClickBait} /> )}
             </div>
-            <div className="small-6 medium-3 columns other-article">
-              <a href="#">
-                <img src="https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg" alt="mountain" />
-                <p>You won't believe what's under this mountain</p>
-              </a>
-            </div>
-            <div className="small-6 medium-3 columns other-article">
-              <a href="#">
-                <img src="http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg" alt="gold" />
-                <p>Mine 20% more gold with One Weird Trick</p>
-              </a>
-            </div>
-            <div className="small-6 medium-3 columns other-article">
-              <a href="#">
-                <img src="http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg" alt="hobbit" />
-                <p>Surprise for Indiana Hobbits born before 1999</p>
-              </a>
-            </div>
-          </div>
         )
     }
 }
